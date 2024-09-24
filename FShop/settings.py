366892ee2554d2aa131f3d95e637d4227e927a04
Fumuri <muhfakhri30@gmail.com>
2024-09-24 10:50:57 +0700

    Push coding tugas 4

--- FShop/settings.py
+++ FShop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)-r9on+kk35#8e&1w+0l^nx0ap&u#8onzmxd4)!8n^sbk^w08@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-fakhri37-fshop.pbp.cs.ui.ac.id"]
 
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from main.views import show_main
 from main.views import create_product
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
 from main.views import show_xml
 from main.views import show_json
 from main.views import show_json_by_id
@@ -11,6 +14,9 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
